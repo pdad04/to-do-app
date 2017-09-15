54f689cda3947be668101d5858e8f8986ebebec8
Andre Dean <Andre@Andres-Retina-MacBook-Pro.local>
2017-09-14 20:47:30 -0700

    Add delete button to delete items

--- app.js
+++ app.js
@@ -12,11 +12,14 @@ function onReady() {
     // create a new li
     let newLi = document.createElement('li');
 
-    // create a new input for the checkbox
+    // create a new input for the checkbox and set it's type
     let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
     checkbox.type = "checkbox";
+
+    // create a delete button.
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'btn-delete';
 
     // set the title
     newLi.textContent = title;
@@ -27,9 +30,20 @@ function onReady() {
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    // attach the delete button to the li
+    newLi.appendChild(deleteButton);
+
+
     // empty the input
     newToDoText.value = '';
   });
+
+  toDoList.addEventListener('click', (event) => {
+    const clickedElement = event.target;
+    if(clickedElement.classList.contains('btn-delete')) {
+      toDoList.removeChild(clickedElement.parentElement);
+    }
+  })
 }
 
 window.onload = function() {
